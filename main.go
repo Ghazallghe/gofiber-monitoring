b02667d9530d31d3fabf4743efb03b4ba4492c28
Ghazaal <ghazaal.laghaee@gmail.com>
2022-09-14 17:44:29 +0430

    adding endpoint set up

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/Ghazallghe/gofiber-monitoring/pkg/configs"
 	"github.com/Ghazallghe/gofiber-monitoring/pkg/db"
+	"github.com/Ghazallghe/gofiber-monitoring/pkg/routes"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -13,6 +14,8 @@ func main() {
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
 	})
+
+	routes.SetUpRoutes(app)
 
 	app.Listen(":3000")
 }

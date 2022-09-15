7401433427b2dc9ef1ea5c4e1bfde1a254bbf0a2
Ghazaal <ghazaal.laghaee@gmail.com>
2022-09-16 00:10:03 +0430

    adding an endpoint for testing token

--- pkg/controllers/generate_token.go
+++ pkg/controllers/generate_token.go
@@ -5,6 +5,7 @@ import (
 	"github.com/Ghazallghe/gofiber-monitoring/pkg/models"
 	"github.com/Ghazallghe/gofiber-monitoring/pkg/utils"
 	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt/v4"
 )
 
 func GenerateToken(c *fiber.Ctx) error {
@@ -15,31 +16,52 @@ func GenerateToken(c *fiber.Ctx) error {
 
 	input := new(inputData)
 	if err := c.BodyParser(input); err != nil {
-		return c.Status(fiber.StatusBadRequest).JSON(err.Error())
+		status := fiber.StatusBadRequest
+		return c.Status(status).JSON(utils.LogicalErrorHandling(status, err.Error()))
 	}
 
 	if err := utils.ValidateStruct(input); err != nil {
-		return c.Status(fiber.StatusBadRequest).JSON(err)
+		errType := fiber.ErrBadRequest
+		return c.Status(errType.Code).JSON(utils.ValidatorErrorHandling(errType.Code, errType.Message, err))
 	}
 
 	user := new(models.User)
 	result := db.DB.Find(&user, "email = ?", input.Email)
 	if result.Error != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(result.Error)
+		status := fiber.StatusBadRequest
+		return c.Status(status).JSON(utils.LogicalErrorHandling(status, result.Error.Error()))
 	}
 
 	if result.RowsAffected == 0 {
-		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"message": "Email not found"})
+		status := fiber.StatusNotFound
+		return c.Status(status).JSON(utils.LogicalErrorHandling(status, "Email Not found"))
 	}
 
 	if err := user.CheckPasswordHash(input.Password); err != nil {
-		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"message": "Email or Password is Not correct"})
+		status := fiber.StatusUnauthorized
+		return c.Status(status).JSON(utils.LogicalErrorHandling(status, "Email or Password is incorrect"))
 	}
 
 	t, err := utils.JwtGenerator(*user)
 	if err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(err.Error())
+		status := fiber.StatusInternalServerError
+		return c.Status(status).JSON(utils.LogicalErrorHandling(status, err.Error()))
 	}
 
 	return c.Status(fiber.StatusOK).JSON(fiber.Map{"token": t})
+}
+
+func TestToken(c *fiber.Ctx) error {
+	user := c.Locals("user").(*jwt.Token)
+	claims := user.Claims.(jwt.MapClaims)
+	id := claims["user_id"].(string)
+
+	dbUser := new(models.User)
+	result := db.DB.Find(dbUser, "id = ?", id)
+	if result.Error != nil {
+		status := fiber.StatusUnauthorized
+		return c.Status(status).JSON(utils.LogicalErrorHandling(status, result.Error.Error()))
+	}
+
+	return c.Status(fiber.StatusOK).JSON(dbUser)
 }
--- pkg/routes/routes.go
+++ pkg/routes/routes.go
@@ -1,6 +1,7 @@
 package routes
 
 import (
+	"github.com/Ghazallghe/gofiber-monitoring/pkg/configs"
 	"github.com/Ghazallghe/gofiber-monitoring/pkg/controllers"
 	"github.com/gofiber/fiber/v2"
 )
@@ -13,5 +14,8 @@ func SetUpRoutes(app *fiber.App) {
 		user := r.Group("/users")
 		user.Post("", controllers.CreateUser)
 		user.Post("/token", controllers.GenerateToken)
+		// test token
+		user.Use(configs.JwtConfig)
+		user.Get("/me", controllers.TestToken)
 	}
 }

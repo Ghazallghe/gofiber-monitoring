5cd44ae761c4a297fc35fa1fe3756f0e2420bf99
Mohammad Shabani <mohamad.darghah@gmail.com>
2022-09-20 20:09:44 +0430

    init alert model

--- pkg/models/url.go
+++ pkg/models/url.go
@@ -5,7 +5,7 @@ import "github.com/google/uuid"
 type Url struct {
 	BaseModel
 	Url        string `json:"url"`
-	Threshold  int32  `json:"threshold"`
+	Threshold  uint   `json:"threshold"`
 	UserId     uuid.UUID
 	Statistics []Statistics
 }
